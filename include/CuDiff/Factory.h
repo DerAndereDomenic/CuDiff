3d12255ddc1ffc25bc51feb156bd4280661b0d98
Domenic Zingsheim <domeniczingsheim@gmail.com>
2025-11-21 15:15:26 +0100

    Move make_variables_impl into struct

--- include/CuDiff/Factory.h
+++ include/CuDiff/Factory.h
@@ -8,18 +8,24 @@
 namespace CuDiff
 {
 
-template<int N, typename... Ts, std::size_t... I>
-CUDIFF_HOSTDEVICE auto
-make_variables_impl(std::index_sequence<I...>, const size_t starts[sizeof...(Ts)], Ts&&... values)
+namespace impl
 {
-    return CuDiff::make_tuple(Dual<N, std::decay_t<Ts>>(std::forward<Ts>(values), starts[I])...);
-}
+template<int N>
+struct make_variables_impl
+{
+    template<typename... Ts, std::size_t... Is>
+    CUDIFF_HOSTDEVICE static auto apply(std::index_sequence<Is...>, const size_t starts[sizeof...(Ts)], Ts&&... values)
+    {
+        return CuDiff::make_tuple(Dual<N, std::decay_t<Ts>>(std::forward<Ts>(values), starts[Is])...);
+    }
+};
+}    // namespace impl
 
 template<int N, typename... Ts>
 CUDIFF_HOSTDEVICE auto make_variables(Ts&&... values)
 {
     VarLayout layout;
     size_t starts[sizeof...(Ts)] = {layout.template alloc<std::decay_t<Ts>>()...};
-    return make_variables_impl<N>(std::index_sequence_for<Ts...> {}, starts, std::forward<Ts>(values)...);
+    return impl::make_variables_impl<N>::apply(std::index_sequence_for<Ts...> {}, starts, std::forward<Ts>(values)...);
 }
 }    // namespace CuDiff
